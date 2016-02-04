72cdeda747b4e9322964fce7e1b17ae3a4745fef
Oleg Bulkin <o.bulkin@gmail.com>
2016-02-04 02:36:49 +0000

    Updated quote column type, seeded quotes, and added quote randomization.

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,4 +1,6 @@
 class QuotesController < ApplicationController
 	def index
+		random_offset = rand(Quote.count)
+		@quote = Quote.offset(random_offset).first
 	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160203194637) do
+ActiveRecord::Schema.define(version: 20160203221216) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "quotes", force: true do |t|
-    t.string   "quote"
+    t.text     "quote"
     t.string   "author"
     t.datetime "created_at"
     t.datetime "updated_at"
